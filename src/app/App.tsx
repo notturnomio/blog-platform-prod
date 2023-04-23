3dd8c6354403545ef3e2d1e1e5228eae18537bb3
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2023-04-24 01:28:52 +0200

    feature-sliced design structure, absolute imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,12 @@ import { Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
-import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
-import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import { AboutPage } from "../pages/AboutPage";
+import { MainPage } from "../pages/MainPage";
+
+// import { useTheme } from "./providers/ThemeProvider";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -21,8 +23,8 @@ const App = () => {
       <Link to={"/about"}>About Page</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/about"} element={<AboutPageLazy />} />
-          <Route path={"/"} element={<MainPageLazy />} />
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
