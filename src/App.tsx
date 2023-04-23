e61e8b5636963a4e341a92c83635cbfa9caadbe1
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2023-04-23 21:57:20 +0200

    added conditional classNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import "./styles/index.scss";
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <div>
         <button onClick={toggleTheme}>
           {theme === "dark" ? "Light Theme" : "Dark Theme"}
