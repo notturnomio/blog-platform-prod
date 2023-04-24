557db60449b5502f1b0f459d0787d12d669413ba
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2023-04-24 16:18:34 +0200

    added router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
 import { AboutPage } from "../pages/AboutPage";
 import { MainPage } from "../pages/MainPage";
+import { AppRouter } from "./providers/router";
 
 // import { useTheme } from "./providers/ThemeProvider";
 
@@ -21,12 +22,7 @@ const App = () => {
       </div>
       <Link to={"/"}>Main Page</Link>
       <Link to={"/about"}>About Page</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,23 @@
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "../routeConfig/routeConfig";
+
+const AppRouter = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {Object.values(routeConfig).map(({ path, element }) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <Suspense fallback={<div>Loading...</div>}>{element}</Suspense>
+            }
+          />
+        ))}
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default AppRouter;
