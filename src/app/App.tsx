0f93299bccfdc9a9a014ffb978727e1da4fe1460
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2023-06-01 17:35:45 +0200

    updated imports to App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import './styles/index.scss';
-import { Sidebar } from 'widgets/Sidebar1';
+import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
 import { classNames } from '../shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
